8f56cd92c7549651705cd6a977fa98cd724f1072
Elton Bautista <eltonbestaha@gmail.com>
2022-08-04 13:41:41 -0600

    Deleted unnecessary boilerplate

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+// LEAVE:
+// <!-- <nav>
+// <RouterLink to="/">Home</RouterLink>
+// <RouterLink to="/about">About</RouterLink>
+// </nav> -->
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,14 +14,6 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
     },
   ],
 });
