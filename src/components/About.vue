6dd20fce7c13453b3700eb8882daa2e2d98c068a
Bobby Lee <superbahbi@gmail.com>
2022-08-04 19:45:40 -0700

    added about cpomonecomponentnt
    added doggo about assets. :3

---
+++ src/components/About.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import image from "../assets/DoggoAbout.png"
+</script>
+
+<template>
+    <section class="container">
+
+        <div class="about-header">
+            <p>
+                <span class="top-span">Here at <em>Groomify</em> we take <em>pride</em> in the work that we
+                    do.</span><br />
+                <span class="bottom-span">We <em>fur-mly</em> believe that trust is the foundation of any good
+                    relationship..</span>
+            </p>
+        </div>
+        <div class="about-subheader">
+            <div class="about-img">
+                <img :src="image" />
+            </div>
+            <div class="about-text">
+                <p>And that is the reason we create time out of our days and make sure to meet our new clients!</p>
+                <p>We’re pet owners, so we know how important your furry friends are to you - trust us when we say;
+                    they’re important to us too.</p>
+            </div>
+        </div>
+
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    height: 100%;
+    margin: auto;
+    background-color: #f1f1f1;
+    padding-top: 2em;
+}
+
+.about-header {
+    display: grid;
+    grid-area: 1/3/2/11;
+    padding-bottom: 2em;
+    gap: 2em;
+
+    p {
+        .top-span {
+            font-size: 2.2em;
+            letter-spacing: 0.032em;
+        }
+
+        .bottom-span {
+            font-size: 1.7em;
+
+            letter-spacing: 0.019em;
+        }
+    }
+
+    em {
+        color: var(--color-text-accent);
+    }
+}
+
+.about-subheader {
+    display: grid;
+    grid-area: 2/3/8/11;
+}
+
+.about-img {
+    display: grid;
+    grid-area: 1/1;
+    padding-top: 6em;
+
+    img {
+        height: 25em
+    }
+}
+
+.about-text {
+    display: grid;
+    grid-area: 1/2;
+    max-width: 300px;
+
+    p {
+        font-size: 2em;
+        padding-bottom: 2em;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
+import About from "@/components/About.vue";
 </script>
 <!-- Please add About and Pricing components just under <TheWelcome />. Make sure to follow TheWelcome's syntax! -->
 <template>
   <main>
     <TheWelcome />
   </main>
+  <main>
+    <About />
+  </main>
 </template>
 
 <style lang="scss" scoped>
 main {
   display: grid;
-  min-height: 100%;
 }
 </style>
