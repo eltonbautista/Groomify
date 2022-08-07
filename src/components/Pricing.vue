60d585809a50802f32015935fffc1ca17294d2ce
Elton Bautista <eltonbestaha@gmail.com>
2022-08-07 12:04:41 -0600

    Created Pricing view

---
+++ src/components/Pricing.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    buttonText: String,
+    backgroundColor: String,
+    textColor: String,
+  },
+});
+</script>
+<template>
+  <section class="container">
+    <div class="grid"></div>
+  </section>
+</template>
+<style lang="scss" scoped>
+.container {
+  background-color: var(--bg-color-v1);
+  width: 100%;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(12, 1fr);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import TheWelcome from "@/components/TheWelcome.vue";
 import About from "@/components/About.vue";
+import Pricing from "../components/Pricing.vue";
 </script>
 <!-- Please add About and Pricing components just under <TheWelcome />. Make sure to follow TheWelcome's syntax! -->
 <template>
   <main>
     <TheWelcome />
-  </main>
-  <main>
     <About />
+    <Pricing />
   </main>
 </template>
 
