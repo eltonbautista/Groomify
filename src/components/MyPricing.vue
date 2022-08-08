ce1db5c3612747f081ec84e258fa0f23e2808eeb
Elton Bautista <eltonbestaha@gmail.com>
2022-08-07 19:33:02 -0600

    Created PricingDisplay and styled MyPricing Need to work on responsiveness

--- src/components/MyPricing.vue
+++ src/components/MyPricing.vue
@@ -1,27 +1,117 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import PricingDisplay from "./PricingDisplay.vue";
+import shihtzu from "../assets/shih-tzu.png";
+import husky from "../assets/contact-dog.png";
+import golden from "../assets/golden.png";
 export default defineComponent({
+  data() {
+    return {
+      shihtzu,
+      husky,
+      golden,
+    };
+  },
   props: {
     buttonText: String,
     backgroundColor: String,
     textColor: String,
   },
+  components: { PricingDisplay },
 });
 </script>
 <template>
   <section class="container">
-    <div class="grid">Hi</div>
+    <div class="grid">
+      <header class="banner">
+        <h1>We&nbsp;<em> match </em>&nbsp;our prices!</h1>
+      </header>
+      <div class="border"></div>
+      <PricingDisplay
+        :display-information="{
+          dogSize: 'Small',
+          breeds: {
+            one: 'Bichon, Boston Terrier',
+            two: 'Chihuahua, Pekingese',
+            three: 'Shih Tzu, Yorkshire Terrier',
+          },
+          price: '35',
+        }"
+        :imgSrc="shihtzu"
+        :area="'3/2/7/6'"
+      />
+      <PricingDisplay
+        :display-information="{
+          dogSize: 'Medium',
+          breeds: {
+            one: 'Australian Shepherd, Beagle',
+            two: 'Cocker Spaniel, Vizsla',
+            three: 'Poodle, German Pinscher',
+          },
+          price: '60',
+        }"
+        :imgSrc="golden"
+        :area="'3/6/7/10'"
+      />
+      <PricingDisplay
+        :display-information="{
+          dogSize: 'Large',
+          breeds: {
+            one: 'Samoyed, Golden Retriever',
+            two: 'Dalmatian, Boxer',
+            three: 'German Shepherd, Husky',
+          },
+          price: '80',
+        }"
+        :imgSrc="husky"
+        :area="'3/10/7/14'"
+      />
+    </div>
   </section>
 </template>
 <style lang="scss" scoped>
 .container {
   background-color: var(--bg-color-v1);
-  width: 100%;
+  display: grid;
+  justify-items: center;
 }
 
 .grid {
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(12, 1fr);
+  grid-template-columns: repeat(14, 1fr);
+  grid-template-rows: repeat(7, 1fr);
+  max-width: var(--max-width);
+  gap: 1em;
+  margin-bottom: 3em;
+}
+
+.border {
+  grid-area: 2/1/8/15;
+  height: 80%;
+  justify-self: center;
+  align-self: center;
+  border: 1px solid var(--color-text-sub);
+  border-radius: 0.5em;
+  padding: 5em;
+}
+
+.banner {
+  display: grid;
+  grid-area: 1/1/2/14;
+  margin-top: 2em;
+  height: 6.3125rem;
+
+  > h1 {
+    font-size: var(--font-size-xxl);
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    margin-top: 0.5em;
+
+    > em {
+      font-weight: 900;
+      color: var(--color-text-sub);
+    }
+  }
 }
 </style>
--- src/components/PricingDisplay.vue
+++ src/components/PricingDisplay.vue
@@ -18,23 +18,37 @@ export default defineComponent({
       required: true,
       type: Object as () => IBreeds,
     },
+    imgSrc: String,
+    area: String,
+  },
+  computed: {
+    cssProps() {
+      return {
+        "--img-src": `url(${this.imgSrc})`,
+        "--area": this.area,
+      };
+    },
   },
   components: { MainButton },
 });
 </script>
 <template>
-  <div class="price-display-wrap">
-    <div class="display-image"></div>
-    <h1 class="dog-size-wrap">
-      <span class="size-adjective">{{ displayInformation.dogSize }}</span>
-      <span class="animal-description">Dogs</span>
-    </h1>
-    <p class="breeds-wrap">
-      <span class="breeds-one">{{ displayInformation.breeds.one }}</span>
-      <span class="breeds-two">{{ displayInformation.breeds.two }}</span>
-      <span class="breeds-three">{{ displayInformation.breeds.three }}</span>
-    </p>
-    <h1>Starts from: ${{ displayInformation.price }}</h1>
+  <div class="price-display-wrap" :style="cssProps">
+    <div class="display-image">
+      <img :src="imgSrc" />
+    </div>
+    <div class="dog-size-wrap">
+      <h2 class="size-adjective">{{ displayInformation.dogSize }}</h2>
+      <h2 class="animal-description">Dogs</h2>
+    </div>
+    <div class="breeds-wrap">
+      <p class="breeds-one">{{ displayInformation.breeds.one }}</p>
+      <p class="breeds-two">{{ displayInformation.breeds.two }}</p>
+      <p class="breeds-three">{{ displayInformation.breeds.three }}</p>
+    </div>
+    <div class="starts-from">
+      <h1>Starts from: ${{ displayInformation.price }}</h1>
+    </div>
     <MainButton
       button-text="Learn More &nbsp; &rarrw;"
       background-color="var(--color-text-main)"
@@ -42,4 +56,67 @@ export default defineComponent({
     />
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.price-display-wrap {
+  grid-area: var(--area);
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  background: var(--bg-color-about);
+  border-radius: 0.5em;
+  box-shadow: 0 0 0.5em 0.2em gray;
+
+  > button {
+    grid-area: 6/1/7/5;
+    height: 70%;
+    width: 80%;
+    display: flex;
+    justify-content: center;
+    justify-self: center;
+    line-height: 3;
+  }
+}
+.display-image {
+  grid-area: 1/1/4/3;
+  > img {
+    max-width: 100%;
+    max-height: 100%;
+    z-index: 5;
+    padding-top: 2em;
+  }
+}
+
+.dog-size-wrap {
+  grid-area: 1/3/4/5;
+  display: grid;
+  justify-items: center;
+  align-content: center;
+
+  > h2 {
+    max-width: fit-content;
+    font-size: calc(var(--font-size-lg) + 1em);
+    font-weight: 500;
+  }
+}
+
+.breeds-wrap {
+  grid-area: 4/1/5/5;
+  display: grid;
+  > p {
+    max-width: fit-content;
+    justify-self: center;
+  }
+}
+
+.starts-from {
+  grid-area: 5/1/6/5;
+  text-align: center;
+  display: grid;
+  align-items: center;
+
+  > h1 {
+    font-size: var(--font-size-lg);
+    font-weight: 500;
+  }
+}
+</style>
