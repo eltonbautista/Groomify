6d25b1019a81550063aeab43401337cbb8c7d83a
Elton Bautista <eltonbestaha@gmail.com>
2022-08-09 13:39:19 -0600

    Created ServiceChild component

---
+++ src/components/ServiceChild.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    text: String,
+  },
+});
+</script>
+<template>
+  <div class="service-child-wrap">
+    <p class="service-child-text"></p>
+  </div>
+</template>
+<style lang="scss" scoped>
+.service-child-wrap {
+  max-width: 100%;
+}
+.service-child-text {
+  font-size: var(--font-size-base);
+}
+</style>
