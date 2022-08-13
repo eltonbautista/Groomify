8b130d33b5ca8dcfd173b24a224b7d3ce4c72cb9
Elton Bautista <eltonbestaha@gmail.com>
2022-08-12 18:13:57 -0600

    style: Created HelcimCard and implemented it in HelcimCarousel
    HelcimCard is a component used to display user's reviews of Groomify's grooming service.

---
+++ src/components/HelcimCard.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    reviewer: {
+      required: true,
+      type: String,
+    },
+    text: {
+      required: true,
+      type: String,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="card">
+    <h3>{{ reviewer }}</h3>
+    <p>{{ text }}</p>
+  </div>
+</template>
+<style lang="scss" scoped></style>
--- src/components/HelcimCarousel.vue
+++ src/components/HelcimCarousel.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import MainButton from "./MainButton.vue";
+import HelcimCard from "./HelcimCard.vue";
 // 1. Initialize
 export default defineComponent({
   data() {
@@ -10,6 +12,22 @@ export default defineComponent({
       count: 2,
       left: 1,
       right: 3,
+      reviews: [
+        { reviewer: "Lori Johnson", text: "5 Stars what an awesome company!" },
+        {
+          reviewer: "Kyle Willems",
+          text: "I absolutely love these guys! They took such great care of Prince!",
+        },
+        {
+          reviewer: "Robert Kugler",
+          text: "Sometimes I don't even feel like taking a shower, but these guys could definitely get me through those times!",
+        },
+        {
+          reviewer: "Elton Bautista",
+          text: "I love the energy just as much as Packer did!!",
+        },
+        { reviewer: "Robert Downey Jr.", text: "Keep going! Never give up!!" },
+      ],
     };
   },
   methods: {
@@ -44,10 +62,27 @@ export default defineComponent({
       }
     },
   },
+  components: { HelcimCard },
 });
 </script>
 <template>
-  <div>L: {{ cards[left] }} C: {{ cards[count] }} R: {{ cards[right] }}</div>
-  <button @click="carousel('next')">Increment</button>
-  <button @click="carousel('previous')">Decrement</button>
+  <div class="carousel-wrapper">
+    <HelcimCard :reviewer="reviews[left].reviewer" :text="reviews[left].text" />
+    <HelcimCard
+      :reviewer="reviews[count].reviewer"
+      :text="reviews[count].text"
+    />
+    <HelcimCard
+      :reviewer="reviews[right].reviewer"
+      :text="reviews[right].text"
+    />
+    <button @click="carousel('previous')" type="button">Previous</button>
+    <button @click="carousel('next')" type="button">Next</button>
+  </div>
 </template>
+<style lang="scss" scoped>
+.carousel-wrapper {
+  display: grid;
+  grid-auto-flow: column;
+}
+</style>
