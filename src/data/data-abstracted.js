a45c526ea690f293b5061dc0c1e29b59cf629974
Elton Bautista <eltonbestaha@gmail.com>
2022-08-13 12:36:56 -0600

    refactor: :art: Abstracted PricingCard data to a separate module for cleanliness

---
+++ src/data/data-abstracted.js
@@ -0,0 +1,31 @@
+const pcd = [
+  {
+    dogSize: "Small",
+    breeds: {
+      one: "Bichon, Boston Terrier",
+      two: "Chihuahua, Pekingese",
+      three: "Shih Tzu, Yorkshire Terrier",
+    },
+    price: "35",
+  },
+  {
+    dogSize: "Medium",
+    breeds: {
+      one: "Australian Shepherd, Beagle",
+      two: "Cocker Spaniel, Vizsla",
+      three: "Poodle, German Pinscher",
+    },
+    price: "45",
+  },
+  {
+    dogSize: "Large",
+    breeds: {
+      one: "Samoyed, Golden Retriever",
+      two: "Dalmatian, Boxer",
+      three: "German Shepherd, Husky",
+    },
+    price: "50",
+  },
+];
+
+export { pcd };
